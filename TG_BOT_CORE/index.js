2b749fb09d890165915bc301a8ce03d4857ed065
wertyullk <huuseynov50@gmail.com>
2026-03-30 00:28:59 +0300

    Add files via upload

---
+++ TG_BOT_CORE/index.js
@@ -0,0 +1,9 @@
+﻿const { Bot } = require('grammy');
+const bot = new Bot(process.env.BOT_TOKEN);
+
+bot.command('start', (ctx) => ctx.reply('🚀 Ядро системы запущено и работает!'));
+
+bot.start({
+    drop_pending_updates: true,
+    onStart: (info) => console.log('✅ БОТ ОЖИЛ: @' + info.username)
+}).catch(err => console.error('Ошибка:', err));
